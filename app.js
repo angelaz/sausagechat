f0c22031e78036c56b85a50bec5a85fdd6552ef9
Sashko Stubailo <sashko@mit.edu>
2014-01-08 21:47:23 -0800

    Added sausage

--- app.js
+++ app.js
@@ -18,9 +18,14 @@ if (Meteor.isClient) {
                 if (Meteor.user()) name = Meteor.user().username;
 
                 if (message.val() != '') {
+                    var messageText = message.val();
+
+                    messageText = messageText.replace(":sausage:",
+                        "<img src='http://media.morristechnology.com/mediafilesvr/upload/coastalcourier/article/2013/01/16/hotdog_.jpg' width=40 height=30 />");
+
                     Messages.insert({
                         name: name,
-                        message: message.val(),
+                        message: messageText,
                         createdAt: Date.now()
                     });
 
