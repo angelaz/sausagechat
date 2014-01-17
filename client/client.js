1f3f269d6622ad51e9169c6ed0e6a0334164fb97
Sashko Stubailo <sashko@mit.edu>
2014-01-17 00:21:13 -0800

    Added XSS vector and automatic link parsing

--- client/client.js
+++ client/client.js
@@ -61,9 +61,30 @@ function escapeHtml(string) {
     });
 }
 
+function linkify(inputText) {
+    var replacedText, replacePattern1, replacePattern2, replacePattern3;
+
+    //URLs starting with http://, https://, or ftp://
+    replacePattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
+    replacedText = inputText.replace(replacePattern1, '<a href="$1" target="_blank">$1</a>');
+
+    //URLs starting with "www." (without // before it, or it'd re-link the ones done above).
+    replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
+    replacedText = replacedText.replace(replacePattern2, '$1<a href="http://$2" target="_blank">$2</a>');
+
+    //Change email addresses to mailto:: links.
+    replacePattern3 = /(([a-zA-Z0-9\-\_\.])+@[a-zA-Z\_]+?(\.[a-zA-Z]{2,6})+)/gim;
+    replacedText = replacedText.replace(replacePattern3, '<a href="mailto:$1">$1</a>');
+
+    return replacedText;
+}
+
 Template.message.sexyMessage = function () {
-    var escaped = escapeHtml(this.message);
-    var sexyMsg = escaped.replace(/\\blob/g,
+    // TODO this allows XSS and stuff
+
+    var sexyMsg = this.message;
+    sexyMsg = linkify(sexyMsg);
+    sexyMsg = sexyMsg.replace(/\\blob/g,
         "<img src='http://4.bp.blogspot.com/-i8HuUVAX4V0/T06jb7mD2MI/AAAAAAAAAGU/k37huhvENLg/s1600/blob.gif' width=40 height=30 />");
     sexyMsg = sexyMsg.replace(/\\sausage/g,
         "<img src='http://media.morristechnology.com/mediafilesvr/upload/coastalcourier/article/2013/01/16/hotdog_.jpg' width=40 height=30 />");
